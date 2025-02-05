ea9d32fdf676ad9e34a089bf3ec13fdac3eb9ad3
liren <327381633@qq.com>
2025-02-05 22:51:13 +0800

    first version of header component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,15 +1,16 @@
-import Logo from "./logo";
 import Link from "next/link";
+import Logo from "./logo";
 
 export default function Header() {
   return (
-    <header>
+    <header className="flex items-center justify-between h-32 p-4 bg-gray-100">
       <Logo />
-      <ul>
+
+      <ul className="flex space-x-4 text-2xl font-semibold">
         <li>
-          <Link href="#projects" />
+          <Link href="#projects">Projects</Link>
         </li>
-        <li>Dark mode</li>
+        <li>Dark Mode</li>
       </ul>
     </header>
   );
--- app/_components/logo.tsx
+++ app/_components/logo.tsx
@@ -2,5 +2,16 @@ import Image from "next/image";
 import logo from "@/public/logo.png";
 
 export default function Logo() {
-  return <Image src={logo} alt="logo image" />;
+  return (
+    <div className="h-full flex items-center">
+      <Image
+        src={logo}
+        alt="logo image"
+        height={64}
+        width={0}
+        className="h-full w-auto object-contain rounded-lg"
+        priority
+      />
+    </div>
+  );
 }
